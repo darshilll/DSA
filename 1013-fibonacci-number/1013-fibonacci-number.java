91dc63eddefe5a4c33240f2d63530c5a4e681e8d
Darshil Gajul <121224494+darshilll@users.noreply.github.com>
2024-12-19 13:59:07 +0530

    Time: 9 ms (24.54%), Space: 40.5 MB (27.34%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public int fib(int n) {
-        if(n <= 1){
-            return n;
-        }
-        int last = fib(n - 1);
-        int slast = fib(n - 2);
-        return last + slast;
+        if(n <= 1) return n;
+
+        int lastD = fib(n -1);
+        int secLastD = fib( n -2);
+        return lastD + secLastD;
     }
 }
