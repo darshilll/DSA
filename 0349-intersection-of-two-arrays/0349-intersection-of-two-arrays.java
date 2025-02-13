2a15ee95cdf44fd2f17c6b361c5ec24ee199068b
Darshil Gajul <121224494+darshilll@users.noreply.github.com>
2025-02-14 02:39:02 +0530

    Time: 2 ms (96.74%), Space: 43.4 MB (48%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,29 +1,47 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        ArrayList<Integer> ans = new ArrayList<>(); 
+        // ArrayList<Integer> ans = new ArrayList<>(); 
 
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
 
-        int i = 0, j = 0;
+        // int i = 0, j = 0;
 
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
-                i++;
-            } else if (nums1[i] > nums2[j]) {
-                j++;
-            } else { 
-                if (ans.isEmpty() || ans.get(ans.size() - 1) != nums1[i]) { 
-                    ans.add(nums1[i]);
-                }
-                i++;
-                j++;
+        // while (i < nums1.length && j < nums2.length) {
+        //     if (nums1[i] < nums2[j]) {
+        //         i++;
+        //     } else if (nums1[i] > nums2[j]) {
+        //         j++;
+        //     } else { 
+        //         if (ans.isEmpty() || ans.get(ans.size() - 1) != nums1[i]) { 
+        //             ans.add(nums1[i]);
+        //         }
+        //         i++;
+        //         j++;
+        //     }
+        // }
+
+        // int[] result = new int[ans.size()];
+        // for (int k = 0; k < ans.size(); k++) {
+        //     result[k] = ans.get(k);
+        // }
+
+        // return result;
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> intersection = new HashSet<>();
+    
+        for(int i : nums1){
+            set.add(i);
+        }
+        for(int j : nums2){
+            if(set.contains(j)){
+                intersection.add(j);
             }
         }
-
-        int[] result = new int[ans.size()];
-        for (int k = 0; k < ans.size(); k++) {
-            result[k] = ans.get(k);
+       int[] result = new int[intersection.size()];
+        int index = 0;
+        for (int num : intersection) {
+            result[index++] = num;
         }
 
         return result;
