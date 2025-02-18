a26a28471414eb09bd0baf9d29a42b182a047f18
Darshil Gajul <121224494+darshilll@users.noreply.github.com>
2025-02-18 17:48:19 +0530

    Time: 0 ms (100%), Space: 43.1 MB (55.9%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,38 +1,31 @@
-public class Solution {
+class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int i = n - 2;
-        
-        // Step 1: Find the first decreasing element
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-        
-        // Step 2: If such element exists, find the smallest element larger than nums[i]
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
+        int i = n -2;
+        for(;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                break;
             }
-            // Step 3: Swap the elements
-            swap(nums, i, j);
         }
-        
-        // Step 4: Reverse the elements after index i
-        reverse(nums, i + 1, n - 1);
+        if(i>=0){
+            int j = n -1;
+            for(;j>=0;j--){
+                if(nums[j] > nums[i]){
+                    break;
+                }
+            }
+                swap(nums,i,j);
+        }
+        reverse(nums,i+1,n-1);
     }
-    
-    // Helper function to swap two elements
-    private void swap(int[] nums, int i, int j) {
+    public static void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-    
-    // Helper function to reverse the array from index `start` to `end`
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start, end);
+    public static void reverse(int[] nums, int start, int end){
+        while(start < end){
+            swap(nums,start,end);
             start++;
             end--;
         }
